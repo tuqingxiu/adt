ba1afe757b8ecf80a6498ddf289537d7b663ffca
tuqingxiu <631795289@qq.com>
2019-01-13 23:30:31 +0800

    11

---
+++ js/common.js
@@ -0,0 +1,15 @@
+(function($) {
+    function loadHeader(){
+        $("#headerBox").load("common/header.html",function(){
+            var current = $(this).attr('data-type');
+            $('header').find('[data-type='+current+']').addClass('current');
+        });
+        $("#footerBox").load("common/footer.html");
+        
+    }
+
+
+    $(window).on('load', function() {
+		loadHeader();
+	});	
+})(window.jQuery);
