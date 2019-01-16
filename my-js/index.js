860ed0cd51abd84e907dc55197a6dea69d79ea7a
涂青秀 <631795289@qq.com>
2019-01-16 17:19:30 +0800

    首页

--- my-js/index.js
+++ my-js/index.js
@@ -8,7 +8,7 @@ $(function() {
 		// pagination: '#slide-swiper',
 		// paginationClickable: true
 	});
-    
+    var allSwiperCount = 8;//swiper总个数
     var preTranslateValue = -1;
 	var translateFlag = false;
 	var index=0;
@@ -16,6 +16,11 @@ $(function() {
         index=localStorage.getItem("index");
         localStorage.removeItem("index"); 
     }
+    //初始化第一个slide的动画
+    $('.slide1 .animated[data-my-animate]').each(function(){
+        var aniName = $(this).attr('data-my-animate');
+        $(this).addClass(aniName);
+    })
 	mySwiper = new Swiper('#index-swiper', {
 		direction: 'vertical',
 		speed: 600,
@@ -23,97 +28,83 @@ $(function() {
 		pagination: '#page',
 		paginationClickable: true,
 		mousewheelControl: true,
-		keyboardControl : true,
+        keyboardControl : true,
 		onScroll: function(swiper) {
-			if(swiper.activeIndex == 7 && preTranslateValue == -1) {
+			if(swiper.activeIndex == allSwiperCount && preTranslateValue == -1) {
 				preTranslateValue = mySwiper.height - $(".all-footer").outerHeight() + mySwiper.translate;
 			}
-			if(swiper.activeIndex == 7 && translateFlag == false) {
+			if(swiper.activeIndex == allSwiperCount && translateFlag == false) {
 				translateFlag = true;
 				swiper.setWrapperTranslate(preTranslateValue);
 				mySwiper.lockSwipeToNext()
 			}
-			if(swiper.activeIndex != 7) {
+			if(swiper.activeIndex != allSwiperCount) {
 				mySwiper.unlockSwipes()
 				translateFlag = false;
 			}
 		},
 		onKeyPress: function(swiper){
-			if(swiper.activeIndex == 7 && preTranslateValue == -1) {
+			if(swiper.activeIndex == allSwiperCount && preTranslateValue == -1) {
 				preTranslateValue = mySwiper.height - $(".all-footer").outerHeight() + mySwiper.translate;
 			}
-			if(swiper.activeIndex == 7 && translateFlag == false) {
+			if(swiper.activeIndex == allSwiperCount && translateFlag == false) {
 				translateFlag = true;
 				swiper.setWrapperTranslate(preTranslateValue);
 				mySwiper.lockSwipeToNext()
 			}
-			if(swiper.activeIndex != 7) {
+			if(swiper.activeIndex != allSwiperCount) {
 				mySwiper.unlockSwipes()
 				translateFlag = false;
 			}
 		},
 		onSlideChangeStart: function(swiper) {
-			var topbar = $('#topbar'),
-				nav_menu = $('#nav_menu'),
-				pagiBullet = $('#page .swiper-pagination-bullet'),
-				num = swiper.activeIndex + 1;
-
-			if(num != 1) {
-				topbar.slideUp();
-				nav_menu.addClass('nav_menu-change');
-				$('#menu li').addClass('black');
-				$('#menu li.active').removeClass('active-b');
-				$('#bs').removeClass('bs-border');
-				$('.logo-blue').show();
-				$('.logo-white').hide();
-				$('.mobile-header').addClass('bgfff')
-			} else {
-				topbar.slideDown();
-				nav_menu.removeClass('nav_menu-change')
-				$('#menu li').removeClass('black');
-				$('#menu li.active').addClass('active-b');
-				$('#bs').addClass('bs-border');
-				$('.logo-blue').hide();
-				$('.logo-white').show();
-				$('.mobile-header').removeClass('bgfff')
-			}
-		
-			if(num == 5) {
-				$('.seven').addClass('fadeInUp-seven');
-			}
-			(num == 8) ? $('#page').css('opacity',0): $('#page').css('opacity',1);
-		},
-
-		onSlideChangeEnd: function(swiper) {
+            //加载动画样式
 			var num = swiper.activeIndex + 1;
-			if(num == 2) {
-				$('.dec-ch').addClass('fadeInUp-created-title');
-			}
-			if(num == 3) {
-				$('.created-title,.seat-title').addClass('fadeInUp-created-title');
-			}
-			if(num == 4 && scrollTrue) {
-				$('.global').addClass('global-fadedown');
-				$('.global-map').addClass('scale-g');
-				$('.china-map').addClass('scale-c');
-				$('.thailand').addClass('scale-t');
-
-				// countNumber();
-				scrollTrue = false;
-			}
-			if(num == 6) {
-				$('.cultrue-subtitle').addClass('fadeInUp-created-title');
-				$('.name').addClass('fadeInUp-created-title');
-				$('.social-name').addClass('fadeInUp-created-title');
-			}
-			if(num == 7) {
-				$('.dream-title-ch').addClass('fadeInUp-created-title');
-			}
+            var activeSlideName = '.slide'+num;
+            $(activeSlideName+' .animated[data-my-animate]').each(function(){
+                var aniName = $(this).attr('data-my-animate');
+                $(this).addClass(aniName);
+            })
+        },
+		onSlideChangeEnd: function(swiper) {
+            // //加载动画样式
+			var num = swiper.activeIndex + 1;
+            if(num == 5){
+                $('slide5 .animated[data-my-animate]').each(function(){
+                    var aniName = $(this).attr('data-my-animate');
+                    $(this).addClass(aniName);
+                })
+                zoombig();
+                setTimeout(function(){
+                    zoomin();
+                },1000);
+            }
 		}
     });
-    
+    //-----------每屏动画效果----------
+    //第3屏效果
+    animateSlide3();
+    //地图动画
+    zoomMap();
+    //第6屏效果
+    animateSlide6();
 })
-
+//第3屏效果
+function animateSlide3() {
+    $('.slide3 .point').hover(function(){
+        $(this).find('.point-text').fadeIn(1000);
+    },function(){
+        $(this).find('.point-text').fadeOut(1000);
+    })
+}
+//地图动画
+function zoomMap(){
+    $('.china-map').hover(function(){
+        zoombig()
+    },function(){
+        zoomin();
+    })
+}
 function zoombig() {
 	$('.global-map').addClass('scale-g');
 	$('.china-map').addClass('scale-c');
@@ -125,32 +116,22 @@ function zoomin() {
 	$('.china-map').removeClass('scale-c');
 	$('.thailand').removeClass('scale-t');
 }
+//第6屏效果
+function animateSlide6(){
+    $('.slide6 .img-box').hover(function(){
+        $(this).find('.content-box').fadeOut();
+    },function(){
+        $(this).find('.content-box').fadeIn();
+    })
+}
+
 function jump(n) {
 	mySwiper.slideTo(n,1000,false);
 }
 
-var scrollTrue = true;
-function addClass(ele,className,n){
-	 var winH=$(window).height();//可视窗口的高度
-	 var top=$(window).scrollTop();//可视窗口的滚动高度
-	 var ele_t=$(ele).offset().top;//内容区的top
-	if(ele=='#global'){
-	 	if((top>ele_t-winH*0.2)&&(top<ele_t)){
-			 $('.global').addClass('global-fadedown');
-		 	 $('.global-map').addClass('scale-g');
-			 $('.china-map').addClass('scale-c');
-			 $('.thailand').addClass('scale-t');
-				 if(scrollTrue) {countNumber(); scrollTrue = false;}
-		}
-		 	
-	 }else if(ele=='#m-tit05'){
-	 	if((top>ele_t-winH*0.3)&&(top<ele_t)){
-			$(ele).addClass(className);
-		 	if(scrollTrue) {countNumber();}
-		}
-		 	
-	 }
-	else if((top>ele_t-winH*n)&&(top<ele_t)){
-		 $(ele).addClass(className);
-	 }
- }  
+  //数字动画
+var countNumber = function() {
+	var countNumberId = $('.mytimer');
+	countNumberId.each(count);
+	scrollTrue = false;
+}
